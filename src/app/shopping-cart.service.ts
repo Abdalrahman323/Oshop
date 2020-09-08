b61a215ca6332ed68dcd7ff8a0c10bde540ef247
Abdalrhman <abdalrhmann323@gmail.com>
2020-09-08 15:17:15 +0200

    add- shopping cart service

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,3 +1,4 @@
+import { product } from './models/product';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 
@@ -8,9 +9,27 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  createCart() {
+  private createCart() {
     return this.db.list('/shopping-carts').push({
       dataCreated: new Date().getTime()
     });
+  }
+
+  private getCart(cartId: string) {
+    return this.db.object('/shopping-carts/' + cartId);
+  }
+
+  private async getOrCreateCard(product: product) {
+
+    let CartId = localStorage.getItem('cartId');
+    if (!CartId) {
+      let result = await this.createCart();
+      localStorage.setItem('cartId', result.key)
+      return this.getCart(result.key)
+    }
+  
+      return this.getCart(CartId)
+
+
   }
 }
