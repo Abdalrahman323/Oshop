4a6c83667deaf49b1600d9afbed73e40b678e0fd
Abdalrhman <abdalrhmann323@gmail.com>
2020-09-09 05:12:22 +0200

    Revert "add- shopping cart service"
    This reverts commit b61a215ca6332ed68dcd7ff8a0c10bde540ef247.

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -1,4 +1,3 @@
-import { product } from './models/product';
 import { AngularFireDatabase } from '@angular/fire/database';
 import { Injectable } from '@angular/core';
 
@@ -9,27 +8,9 @@ export class ShoppingCartService {
 
   constructor(private db: AngularFireDatabase) { }
 
-  private createCart() {
+  createCart() {
     return this.db.list('/shopping-carts').push({
       dataCreated: new Date().getTime()
     });
-  }
-
-  private getCart(cartId: string) {
-    return this.db.object('/shopping-carts/' + cartId);
-  }
-
-  private async getOrCreateCard(product: product) {
-
-    let CartId = localStorage.getItem('cartId');
-    if (!CartId) {
-      let result = await this.createCart();
-      localStorage.setItem('cartId', result.key)
-      return this.getCart(result.key)
-    }
-  
-      return this.getCart(CartId)
-
-
   }
 }
