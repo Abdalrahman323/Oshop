2e8f3433c35fa0e99aeaa7e94c1274bc53b56bad
unknown <Abdalrhmann323@gmail.com>
2020-09-13 05:43:45 +0200

    shopping-cart-item better object intialzation

--- src/app/models/shopping-cart-item.ts
+++ src/app/models/shopping-cart-item.ts
@@ -6,6 +6,12 @@ export class ShoppingCartItem {
  imageUrl:string;
  price:number;
  quantity:number;
+// what's that means
+// this means init can be an object that looks the shopping cart item object
+// it can have one or more property
+ constructor (init? : Partial<ShoppingCartItem>){
+     Object.assign(this,init);
+ }
  
  get totalPrice (){  return this.price * this.quantity;}
 }
